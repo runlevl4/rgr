7a22ce75d13e7b3eeb7e5effd9a18f2a48631fd7
runlevl4 <root@runlevl4.com>
2020-09-27 12:38:51 -0400

    Made repeat count dynamic.

--- iteration/iterations.go
+++ iteration/iterations.go
@@ -1,10 +1,10 @@
 package iteration
 
 // Repeat takes an input string and repeats it 5 times.
-func Repeat(x string) string {
+func Repeat(x string, count int) string {
 	var tmp string
 
-	for i := 0; i < 5; i++ {
+	for i := 0; i < count; i++ {
 		tmp += x
 	}
 	return tmp
--- iteration/iterations_test.go
+++ iteration/iterations_test.go
@@ -3,8 +3,8 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	got := Repeat("a")
-	want := "aaaaa"
+	got := Repeat("a", 10)
+	want := "aaaaaaaaaa"
 
 	if got != want {
 		t.Errorf("want %s, got %s", want, got)
@@ -13,6 +13,6 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
