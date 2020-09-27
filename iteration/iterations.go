d853795e673b5ec34517ce28297d09f53583084c
runlevl4 <root@runlevl4.com>
2020-09-27 12:33:28 -0400

    Added comment; refactored Repeat.

--- iteration/iterations.go
+++ iteration/iterations.go
@@ -1,10 +1,11 @@
 package iteration
 
+// Repeat takes an input string and repeats it 5 times.
 func Repeat(x string) string {
 	var tmp string
 
 	for i := 0; i < 5; i++ {
-		tmp = tmp + x
+		tmp += x
 	}
 	return tmp
 }
